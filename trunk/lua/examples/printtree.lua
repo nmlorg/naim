fcdbbdaac9de24775a9f57d075c0707a3c1507ac
Daniel Reed <nmlorg@users.noreply.github.com>
2006-12-17 01:36:25 +0000

    Load naim-0.11.8.3-2006-07-05-1355 into trunk.

--- trunk/lua/examples/printtree.lua
+++ trunk/lua/examples/printtree.lua
@@ -1,22 +1,33 @@
-function naim.printtree(t)
+function naim.printtree(n)
 	visited = {}
 
-	function doprint(t, depth)
+	function doprint(t, n, depth)
+		function printkey(k)
+			if type(k) == "string" then
+				return(k)
+			else
+				return("[" .. tostring(k) .. "]")
+			end
+		end
+
 		for k,v in pairs(t) do
 			if type(v) == "table" then
-				naim.echo(string.rep(" &nbsp;", depth) .. " " .. tostring(k) .. " = {")
+				naim.echo(string.rep(" &nbsp;", depth) .. " " .. printkey(k) .. " = {")
 				if visited[v] == nil then
-					visited[v] = true
-					doprint(v, depth+1)
+					visited[v] = n .. "." .. k
+					doprint(v, visited[v], depth+1)
 				else
-					naim.echo(string.rep(" &nbsp;", depth+1) .. " (already displayed)")
+					naim.echo(string.rep(" &nbsp;", depth+1) .. " (already displayed as " .. visited[v] .. ")")
+					visited[v] = visited[v] .. ", " .. n .. "." .. k
 				end
-				naim.echo(string.rep(" &nbsp;", depth) .. " }")
+				naim.echo(string.rep(" &nbsp;", depth) .. " },")
+			elseif type(v) == "string" then
+				naim.echo(string.rep(" &nbsp;", depth) .. " " .. printkey(k) .. " = \"" .. tostring(v) .. "\",")
 			else
-				naim.echo(string.rep(" &nbsp;", depth) .. " " .. tostring(k) .. " = (" .. type(v) .. ")" .. tostring(v))
+				naim.echo(string.rep(" &nbsp;", depth) .. " " .. printkey(k) .. " = " .. tostring(v) .. ",")
 			end
 		end
 	end
 
-	doprint(t, 0)
+	doprint(_G[n], n, 0)
 end
--- trunk/src/snapshot.h
+++ trunk/src/snapshot.h
@@ -1,1 +1,1 @@
-#define NAIM_SNAPSHOT "-2006-07-03-2337 (development snapshot)"
+#define NAIM_SNAPSHOT "-2006-07-05-1355 (development snapshot)"
