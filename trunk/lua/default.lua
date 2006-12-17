fb3b44c8445ac4a8967ef848229ea863aa40943e
Daniel Reed <nmlorg@users.noreply.github.com>
2006-12-17 01:09:01 +0000

    Load naim-0.11.8.3-2006-06-12-1938 into trunk.

--- trunk/lua/default.lua
+++ trunk/lua/default.lua
@@ -1,13 +1,3 @@
-naim.variables = {}
-naim.commands = {}
-naim.connections = {}
-
-setmetatable(naim.connections, {
-	__newindex = function (t,k,v)
-		error("only naim can update the connections table",2)
-	end
-})
-
 function naim.internal.expandString(s)
 	s = string.gsub(s, "$%((.*)%)",
 		function (n)
@@ -24,100 +14,97 @@ function naim.internal.expandString(s)
 	return s
 end
 
-function naim.internal.newConn(id)
-	-- Hahaha, what a hack.
-	local winname = naim.prototypes.connection.get_winname(id)
+function naim.internal.rwmetatable(prototype)
+	return({
+		__index = function(table, key)
+			if prototype[key] ~= nil then
+				return prototype[key]
+			elseif prototype["get_"..key] ~= nil then
+				return prototype["get_"..key](table.handle)
+			else
+				return nil
+			end
+		end,
+		__newindex = function(table, key, value)
+			if prototype["set_"..key] ~= nil then
+				prototype["set_"..key](table.handle, key, value)
+			elseif prototype["get_"..key] ~= nil then
+				error(key .. " is a read-only attribute",2)
+			else
+				rawset(table, key, value)
+			end
+		end,
+	})
+end
 
+function naim.internal.rometatable(name)
+	return({
+		__newindex = function (t,k,v)
+			error("only naim can update the " .. name .. " table", 2)
+		end
+	})
+end
+
+
+
+naim.variables = {}
+naim.commands = {}
+naim.connections = {}
+
+setmetatable(naim.connections, naim.internal.rometatable("connections"))
+
+function naim.internal.newConn(winname, handle)
 	setmetatable(naim.connections, {})
 	naim.connections[winname] = {
-		id = id,
+		handle = handle,
 		windows = {},
 		buddies = {},
 	}
-	local mt = {
-		__index = function(table, key)
-			if naim.prototypes.connection[key] ~= nil then
-				return naim.prototypes.connection[key]
-			end
-			if naim.prototypes.connection["get_"..key] ~= nil then
-				return naim.prototypes.connection["get_"..key](table.id)	-- XXX: is this really how I want to do this?
-			end
-			return nil
-		end,
-		__newindex = function(table, key, value)
-			if naim.prototypes.connection["set_"..key] ~= nil then
-				naim.prototypes.connection["set_"..key](table.id, key, value)
-				return
-			end
-			table[key] = value
-		end
-	}
-	setmetatable(naim.connections[winname], mt);
-	setmetatable(naim.connections, {
-		__newindex = function (t,k,v)
-			error("only naim can update the connections table",2)
-		end
-	})
+	setmetatable(naim.connections[winname], naim.internal.rwmetatable(naim.prototypes.connections))
+	setmetatable(naim.connections[winname].windows, naim.internal.rometatable("windows"))
+	setmetatable(naim.connections[winname].buddies, naim.internal.rometatable("buddies"))
+	setmetatable(naim.connections, naim.internal.rometatable("connections"))
 end
 
-function naim.internal.delConn(id)
-	local winname = naim.prototypes.connection.get_winname(id)
+function naim.internal.delConn(winname)
 	setmetatable(naim.connections, {})
 	naim.connections[winname] = nil
-	setmetatable(naim.connections, {
-		__newindex = function (t,k,v)
-			error("only naim can update the connections table",2)
-		end
-	})
+	setmetatable(naim.connections, naim.internal.rometatable("connections"))
 end
 
-function naim.internal.newwin(conn, winname)
+function naim.internal.newwin(conn, winname, handle)
 	setmetatable(conn.windows, {})
-	conn.windows[winname] = {}
-	setmetatable(conn.windows, {
-		__newindex = function (t,k,v)
-			error("only naim can update the windows table",2)
-		end
-	})
+	conn.windows[winname] = {
+		handle = handle
+	}
+	setmetatable(conn.windows[winname], naim.internal.rwmetatable(naim.prototypes.windows))
+	setmetatable(conn.windows, naim.internal.rometatable("windows"))
 end
 
 function naim.internal.delwin(conn, winname)
 	setmetatable(conn.windows, {})
 	conn.windows[winname] = nil
-	setmetatable(conn.windows, {
-		__newindex = function (t,k,v)
-			error("only naim can update the windows table",2)
-		end
-	})
+	setmetatable(conn.windows, naim.internal.rometatable("windows"))
 end
 
-function naim.internal.newbuddy(conn, account)
+function naim.internal.newbuddy(conn, account, handle)
 	setmetatable(conn.buddies, {})
-	conn.buddies[account] = {}
-	setmetatable(conn.buddies, {
-		__newindex = function (t,k,v)
-			error("only naim can update the buddies table",2)
-		end
-	})
+	conn.buddies[account] = {
+		handle = handle
+	}
+	setmetatable(conn.buddies[account], naim.internal.rwmetatable(naim.prototypes.buddies))
+	setmetatable(conn.buddies, naim.internal.rometatable("buddies"))
 end
 
 function naim.internal.changebuddy(conn, account, newaccount)
 	setmetatable(conn.buddies, {})
 	conn.buddies[newaccount] = conn.buddies[account]
 	conn.buddies[account] = nil
-	setmetatable(conn.buddies, {
-		__newindex = function (t,k,v)
-			error("only naim can update the buddies table",2)
-		end
-	})
+	setmetatable(conn.buddies, naim.internal.rometatable("buddies"))
 end
 
 function naim.internal.delbuddy(conn, account)
 	setmetatable(conn.buddies, {})
 	conn.buddies[account] = nil
-	setmetatable(conn.buddies, {
-		__newindex = function (t,k,v)
-			error("only naim can update the buddies table",2)
-		end
-	})
+	setmetatable(conn.buddies, naim.internal.rometatable("buddies"))
 end
