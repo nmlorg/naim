7f685208d7409b64f0f180bc3917c34e4870aec7
Andrew Chin <eminence@users.noreply.github.com>
2007-07-11 17:03:22 +0000

    tweaked some of the regular expressions added new !weather <user> command added the ability to get weather in non-US locations

--- contrib/trunk/netlib/weather.lua
+++ contrib/trunk/netlib/weather.lua
@@ -20,18 +20,23 @@ end
 
 function naim.weather.getdata(args)
 	local data = netlib.httpget("weather.yahooapis.com", "/forecastrss?p="..args)
-	city = data:match("<yweather:location.*city=\"([%w/\. ]+)\".*/>")
+	city = data:match("<yweather:location.*city=\"([%w/\. -]+)\".*/>")
 
-	condition = data:match("<yweather:condition.*text=\"([%w/\. ]+)\".*/>")
-	 if condition then tosend = "Currently in " .. city .. ": " .. condition else tosend = "" end
+	weatherdate = data:match("<yweather:condition.*date=\".* ([%d]+:[%d]+ [apm]+ [%w]+)\".*/>")
+	now = ""
+	if weatherdate then now = "As of " .. weatherdate else now = "Currently" end
+	--naim.echo("data: " .. data)
 
+	condition = data:match("<yweather:condition.*text=\"([%w/\. -]+)\".*/>")
+	if condition then tosend = now .. " in " .. city .. ": " .. condition else tosend = now end
+	
 	temp = data:match("<yweather:condition.*temp=\"(%d+)\".*/>")
 	if temp then tosend = tosend .. " temp=" .. temp end
 	tempunit = data:match("<yweather:units.*temperature=\"(%a)\".*/>")
 	if tempunit then tosend = tosend .. tempunit else tempunit = "" end
 
-	windchill = data:match("<yweather:wind.*chill=\"(%d+)\".*/>")
-	if windchill then tosend = tosend .. " windchill=" .. windchill end
+	windchill = data:match("<yweather:wind.*chill=\"([-%d]+)\".*/>")
+	if windchill then tosend = tosend .. " windchill=" .. windchill ..tempunit end
 
 	humidity = data:match("<yweather:atmosphere.*humidity=\"(%d+)\".*/>")
 	if humidity then tosend = tosend .. " humidity=" .. humidity .. "%" end
@@ -41,10 +46,10 @@ function naim.weather.getdata(args)
 	tosend = tosend .. " " .. forecastday .. ":"
 
 	forecastlow = data:match("<yweather:forecast.*low=\"(%d+)\".*/>")
-	if forecastlow then tosend = tosend .. " low=" .. forecastlow end
+	if forecastlow then tosend = tosend .. " low=" .. forecastlow ..tempunit end
 
 	forecasthigh = data:match("<yweather:forecast.*high=\"(%d+)\".*/>")
-	if forecasthigh then tosend = tosend .. " high=" .. forecasthigh end
+	if forecasthigh then tosend = tosend .. " high=" .. forecasthigh ..tempunit end
 
 	forecasttext = data:match("<yweather:forecast.*text=\"([%w/\. ]+)\".*/>")
 	if forecasttext then tosend = tosend .. " " .. forecasttext end
@@ -62,11 +67,19 @@ if naim.weather.hook then naim.hooks.del('proto_recvfrom',naim.weather.hook) nai
 
 naim.weather.hook = naim.hooks.add('proto_recvfrom', function (conn, sn, dest, text, flags)
 	if not dest then rcpt = sn else rcpt = dest end
-	if text:match("^!weather (%d%d%d%d%d)$") then
-		local zip = text:match("^!weather (%d%d%d%d%d)$")
+	if string.sub(text,0,1) == "#" then rcpt = sn end
+	if text:match("^[!#]weather (.*%d%d%d%d)$") then
+		local zip = text:match("^[!#]weather (.*%d%d%d%d)$")
 		naim.weather.history[sn] = zip
 		netlib.createcontext(function() conn:msg(rcpt,naim.weather.getdata(zip) ) end)
-	elseif text:match("^!weather$") then
+	elseif text:match("^[!#]weather ([%w%p]*)$") then
+		local who = text:match("[!#]weather ([%w%p]*)$") 
+		if not naim.weather.history[who] then
+			conn:msg(rcpt,"Who?")
+		else
+			netlib.createcontext(function() conn:msg(rcpt, naim.weather.getdata(naim.weather.history[who])) end)
+		end
+	elseif text:match("^[!#]weather$") then
 		if not naim.weather.history[sn] then
 			conn:msg(rcpt,"What zipcode do you want the weather for?")
 		else
