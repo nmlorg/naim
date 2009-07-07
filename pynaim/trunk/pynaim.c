175e0d44cc8998d08015337d48da5cbdeb94b9c1
nmlorg <nmlorg@93e3c89e-4921-0410-827d-51100a812d08>
2009-07-07 04:03:25 +0000

    Create "naim" module, with single member function "echof". (Patch from Eminence.)

--- pynaim/trunk/pynaim.c
+++ pynaim/trunk/pynaim.c
@@ -1,6 +1,8 @@
 #include <Python.h>
 #include <naim/modutil.h>
 
+extern conn_t *curconn;
+
 static int cmd_pyeval(conn_t *c, const char *cmd, const char *arg) {
 	if (strcasecmp(cmd, "PYEVAL") != 0)
 		return(HOOK_CONTINUE);
@@ -10,8 +12,27 @@ static int cmd_pyeval(conn_t *c, const char *cmd, const char *arg) {
 	return(HOOK_STOP);
 }
 
+static PyObject* pynaim_echof(PyObject *self, PyObject *args) {
+	const char *string;
+
+	if (!PyArg_ParseTuple(args, "s:echof", &string))
+		return(NULL);
+
+	if (curconn) {
+		status_echof(curconn, string);
+		return(Py_BuildValue("i", 0));
+	} else
+		return(NULL);
+}
+
+static PyMethodDef NaimModule[] = {
+	{"echof", pynaim_echof, METH_VARARGS, "Does a status_echof call."},
+	{NULL, NULL, 0, NULL},
+};
+
 int	naim_init(void *mod, const char *str) {
 	Py_Initialize();
+	Py_InitModule("naim", NaimModule);
 
 	HOOK_ADD(getcmd, mod, cmd_pyeval, 100);
 
