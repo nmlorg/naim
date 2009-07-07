05e5af9e34c53ab8adeb8ecaae120db551068c23
nmlorg <nmlorg@93e3c89e-4921-0410-827d-51100a812d08>
2009-07-07 05:19:13 +0000

    Automatically load default.py at modload.
    Rename naim.echof to naim.echo (same as nlua).

--- pynaim/trunk/default.py
+++ pynaim/trunk/default.py
@@ -5,6 +5,6 @@ import naim
 
 def _myexcepthook(exctype, value, tb):
   for l in traceback.format_exception(exctype, value, tb):
-    naim.echof(l)
+    naim.echo(l)
 
 sys.excepthook = _myexcepthook
