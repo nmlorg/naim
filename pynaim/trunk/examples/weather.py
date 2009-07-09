020fb19cd8e5b42d1205c6dcaface278e0527a0c
Andrew Chin <eminence@users.noreply.github.com>
2009-07-09 03:05:00 +0000

    Small cleanups

--- pynaim/trunk/examples/weather.py
+++ pynaim/trunk/examples/weather.py
@@ -20,8 +20,8 @@ except ImportError:
         def __init__(self):
             self.hooks = self.FakeHooks()
         def echo(self, message, *args):
-            if len(args)>0:
-                message = message %(args)
+            if args:
+                message = message % args
             print message
         def eval(self, *args):
             print ",".join(args)
@@ -30,25 +30,26 @@ except ImportError:
 import re
 import httplib
 
+PYWEATHER_RE = re.compile("^!pyweather\s+(\d{5})$")
+YCONDITION_RE = re.compile("<yweather:condition.*?text=\"(.*?)\".*?temp=\"(\d+)\".*?/>")
+
 def weatherRecvFrom(conn, src, dst, message, flags):
     naim.echo("Message from %s: %s", src, message)
     if dst and dst[:1] == "#":
-        r = re.compile("^!pyweather\s+(\d{5})$")
-        m = r.match(message)
+        m = PYWEATHER_RE.match(message)
         if m:
-            zip = m.groups(1)
+            zip = m.group(1)
             conn = httplib.HTTPConnection("weather.yahooapis.com")
-            conn.request("GET","/forecastrss?p=%s"%(zip))
+            conn.request("GET","/forecastrss?p=%s" % zip)
             r = conn.getresponse()
             if r.status == 200:
                 data = r.read()
-                #naim.echo("I have data!: %r"%data)
-                ### TODO it would be nice to actually use an XML parser here, instead of 
-                ###      attempting to regex out the info we want
-                conditionRE = re.compile("<yweather:condition.*?text=\"(.*?)\".*?temp=\"(\d+)\".*?/>")
-                conditionM = conditionRE.search(data)
-                if (conditionM):
-                    naim.eval("/msg %s %s" % (dst,"It is currently %s and %s" % (conditionM.group(2), conditionM.group(1))))
+                #naim.echo("I have data!: %r", data)
+                # TODO(eminence): it would be nice to actually use an XML parser
+                # here, instead of attempting to regex out the info we want
+                conditionM = YCONDITION_RE.search(data)
+                if conditionM:
+                    naim.eval("/msg %s %s" % (dst,"It is currently %s and %s" % conditionM.groups([2,1])))
                 else:
                     naim.echo("failed to find condition match")
             else:
