25fa7b077e887b89edc04f9138ffe5f894b70933
nmlorg <nmlorg@93e3c89e-4921-0410-827d-51100a812d08>
2009-07-09 04:20:16 +0000

    More style cleanup (mostly from http://code.google.com/p/soc/wiki/PythonStyleGuide, but using StudlyCaps for functions and methods as per Google internal style).

--- pynaim/trunk/examples/weather.py
+++ pynaim/trunk/examples/weather.py
@@ -1,65 +1,81 @@
-## Example pynaim script:  weather bot
-##   responds to !pyweather <zipcode> messages in irc channels
-##
-## to test from outside naim, just run:
-##  python weather.py
-## to run from within naim:
-##  /modload path/to/pynaim
-##  /pyload path/to/weather.py
+"""pynaim weather bot (responds to !pyweather <zipcode>).
+
+To test from outside naim, just run:
+  python weather.py
+To run from within naim:
+  /modload path/to/pynaim
+  /pyload path/to/weather.py
+"""
+
+__author__ = 'Andrew Chin (eminence)'
+
+import httplib
+import re
 
 try:
-    import naim
-    ### THESE ARE QUICK HACKS
-    FakeNaim = None
+  import naim
+  ### THESE ARE QUICK HACKS
+  FakeNaim = None
 except ImportError:
-    print "Not running from within naim!  Using FakeNaim"
-    class FakeNaim:
-        class FakeHooks:
-            def add(self,*args,**kwargs):
-                print "Adding fake hook: %s %d %r" % (args[0],args[1], args[2])
-        def __init__(self):
-            self.hooks = self.FakeHooks()
-        def echo(self, message, *args):
-            if args:
-                message = message % args
-            print message
-        def eval(self, *args):
-            print ",".join(args)
-    naim = FakeNaim()
-    
-import re
-import httplib
+  print 'Not running from within naim!  Using FakeNaim'
 
-PYWEATHER_RE = re.compile("^!pyweather\s+(\d{5})$")
-YCONDITION_RE = re.compile("<yweather:condition.*?text=\"(.*?)\".*?temp=\"(\d+)\".*?/>")
+  class FakeNaim(object):
+    class FakeHooks(object):
+      def add(self, name, weight, func):
+        print 'Adding fake hook: %s %i %r' % (name, weight, func)
 
-def weatherRecvFrom(conn, src, dst, message, flags):
-    naim.echo("Message from %s: %s", src, message)
-    if dst and dst[:1] == "#":
-        m = PYWEATHER_RE.match(message)
-        if m:
-            zip = m.group(1)
-            conn = httplib.HTTPConnection("weather.yahooapis.com")
-            conn.request("GET","/forecastrss?p=%s" % zip)
-            r = conn.getresponse()
-            if r.status == 200:
-                data = r.read()
-                #naim.echo("I have data!: %r", data)
-                # TODO(eminence): it would be nice to actually use an XML parser
-                # here, instead of attempting to regex out the info we want
-                conditionM = YCONDITION_RE.search(data)
-                if conditionM:
-                    naim.eval("/msg %s %s" % (dst,"It is currently %s and %s" % conditionM.groups([2,1])))
-                else:
-                    naim.echo("failed to find condition match")
-            else:
-                naim.eval("msg %s %s"%(dst,"Failed to open weather URL!"))
-    pass
-          
+    def __init__(self):
+      self.hooks = self.FakeHooks()
 
-naim.hooks.add('recvfrom', 200, weatherRecvFrom)
+    def echo(self, s, *args):
+      if args:
+        s %= args
+      else:
+        s = '%s' % (s,)
+
+      print s
+
+    def eval(self, s):
+      print s
+
+  naim = FakeNaim()
+
+
+PYWEATHER_RE = re.compile('^!pyweather\s+(\d{5})$')
+YCONDITION_RE = re.compile('<yweather:condition.*?text="(.*?)".*?temp="(\d+)".*?/>')
+
+
+def WeatherRecvFrom(conn, src, dst, message, flags):
+  naim.echo('Message from %s: %s', src, message)
+
+  if not dst or dst[0] != '#':
+    return
+
+  m = PYWEATHER_RE.match(message)
+  if not m:
+    return
+
+  zip = m.group(1)
+  conn = httplib.HTTPConnection('weather.yahooapis.com')
+  conn.request('GET', '/forecastrss?p=%s' % zip)
+  r = conn.getresponse()
+  if r.status != 200:
+    naim.eval('msg %s %s' % (dst, 'Failed to open weather URL!'))
+    return
+
+  data = r.read()
+  #naim.echo('I have data!: %r', data)
+  # TODO(eminence): it would be nice to actually use an XML parser
+  # here, instead of attempting to regex out the info we want
+  conditionM = YCONDITION_RE.search(data)
+  if conditionM:
+    naim.eval('/msg %s %s' % (dst, 'It is currently %s and %s' % conditionM.groups([2, 1])))
+  else:
+    naim.echo('failed to find condition match')
+
+
+naim.hooks.add('recvfrom', 200, WeatherRecvFrom)
 
 ## manual test:
 if FakeNaim:
-    weatherRecvFrom("conn","test","#em32","!pyweather 02906",0)
-
+  WeatherRecvFrom('conn','test', '#em32', '!pyweather 02906', 0)
